cefd7f02f583e7834e7d27949f9064daea1b302b
Leon Lin <lianglin.021@gmail.com>
2021-08-23 21:33:22 -0700

    completed delete tag method in Tags page

--- src/main/java/com/leonplatform/Controller/Admin/TagController.java
+++ src/main/java/com/leonplatform/Controller/Admin/TagController.java
@@ -8,6 +8,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -32,6 +33,12 @@ public class TagController {
         if (t == null) {
             System.out.println("Failed to add tag");
         }
+        return "redirect:/admin/tags";
+    }
+
+    @GetMapping("/tags/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        tagService.deleteTag(id);
         return "redirect:/admin/tags";
     }
 }

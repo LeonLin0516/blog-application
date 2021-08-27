685e1eee5618cff94eaf908e6bd2ce51ef76d3ef
Leon Lin <lianglin.021@gmail.com>
2021-08-27 15:48:29 -0700

    fixed edit tag bugs

--- src/main/java/com/leonplatform/Controller/Admin/TagController.java
+++ src/main/java/com/leonplatform/Controller/Admin/TagController.java
@@ -8,13 +8,11 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
@@ -34,18 +32,31 @@ public class TagController {
 
     @PostMapping("/tags/post")
     public String post(Tag tag, RedirectAttributes redirectAttributes) {
-        if ("".equals(tag.getName())) {
-            redirectAttributes.addFlashAttribute("negativeMessage", "Tag title can't be empty!");
+        if ("".equals(tag.getName().strip())) {
+            redirectAttributes.addFlashAttribute("negativeMessage", "Title Can't Be Empty!");
             return "redirect:/admin/tags";
         } else if (tagService.getTagByName(tag.getName()) != null) {
-            redirectAttributes.addFlashAttribute("negativeMessage", "Tag has already exist!");
+            redirectAttributes.addFlashAttribute("negativeMessage", "Already Exist!");
             return "redirect:/admin/tags";
         }
         Tag t = tagService.saveTag(tag);
         if (t == null) {
-            redirectAttributes.addFlashAttribute("negativeMessage", "Failed to create tag!");
+            redirectAttributes.addFlashAttribute("negativeMessage", "Failed to Create Tag!");
         }
         redirectAttributes.addFlashAttribute("positiveMessage", "Create tag successfully!");
+        return "redirect:/admin/tags";
+    }
+
+    @PostMapping("/tags/{id}/edit")
+    public String edit(@PathVariable Long id, Tag tag,
+                       RedirectAttributes redirectAttributes) {
+        Tag t = tagService.getTagByName(tag.getName());
+        if (t != null) {
+            redirectAttributes.addFlashAttribute("negativeMessage", "Already Exist!");
+        } else {
+            tagService.updateTag(id, tag);
+            redirectAttributes.addFlashAttribute("positiveMessage", "Updated Successfully!");
+        }
         return "redirect:/admin/tags";
     }
 
